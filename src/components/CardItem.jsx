5e32427dd58fef2a0ce848310ec4fa89a77c3187
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2024-02-19 15:51:21 -0800

    Works on styling and responsiveness

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -8,7 +8,7 @@ const CardItem = (props) => {
       <li className="cards--item">
         <Link className="cards--item--link" to={props.path}>
           <figure className="cards--item--pic-wrap" data-category={props.label}>
-            <img src={props.src} alt="TravelImage" className="cards--item--img" />
+            <img src={props.src} alt="Image" className="cards--item--img" />
           </figure>
           <div className="cards--item--info">
             <h5 className="cards--item--text">{props.text}</h5>
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,19 +1,40 @@
 import React from "react";
 import CardItem from "./CardItem";
 import "./Cards.css";
-import img9 from "../assets/images/img-9.jpg"
+import img1 from "../assets/images/img-1.jpg"; // TODO: figure out how to pass props with src showing actual file path
+import img2 from "../assets/images/img-2.jpg";
+import img6 from "../assets/images/img-6.jpg";
+import img9 from "../assets/images/img-9.jpg";
 
 const Cards = () => {
   return (
     <div className="cards">
-      <h1>Check out these EPIC Destinations!</h1>
-      <div className="cards-container">
+      <h1>This is our class!</h1>
+      <div className="cards--container">
         <div className="cards--wrapper">
-          <ul className="cards--item">
+          <ul className="cards--items">
             <CardItem
               src={img9}
               text="Explore the hidden waterfall deep inside the Amazon Rainforest"
               label="Adventure"
+              path="/"
+            />
+            <CardItem
+              src={img1}
+              text="Explore the hidden waterfall deep inside the Amazon Rainforest"
+              label="Adventure"
+              path="/"
+            />
+            <CardItem
+              src={img2}
+              text="Travel through the islands..."
+              label="Adventure"
+              path="/"
+            />
+            <CardItem
+              src={img6}
+              text="Explore the hidden waterfall deep inside the Amazon Rainforest"
+              label="Teacher"
               path="/"
             />
           </ul>
