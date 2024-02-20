10ac576689ad786688462316a3c8d41c7025587b
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2024-02-20 13:41:03 -0800

    Comments out unnecessary parts of footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -29,34 +29,36 @@ const Footer = () => {
         <div className="footer-link-wrapper">
           <div class="footer-link-items">
             <h2>About Us</h2>
-            <Link to="/sign-up">How it works</Link>
+            <Link to="/class">Our Team</Link>
+            {/* <Link to="/sign-up">How it works</Link> */}
             <Link to="/">Testimonials</Link>
             <Link to="/">Careers</Link>
-            <Link to="/">Investors</Link>
+            {/* <Link to="/">Investors</Link> */}
             <Link to="/">Terms of Service</Link>
           </div>
           <div class="footer-link-items">
             <h2>Contact Us</h2>
             <Link to="/">Contact</Link>
             <Link to="/">Support</Link>
-            <Link to="/">Destinations</Link>
+            {/* <Link to="/">Destinations</Link> */}
             <Link to="/">Sponsorships</Link>
           </div>
         </div>
         <div className="footer-link-wrapper">
           <div class="footer-link-items">
             <h2>Videos</h2>
-            <Link to="/">Submit Video</Link>
-            <Link to="/">Ambassadors</Link>
-            <Link to="/">Agency</Link>
-            <Link to="/">Influencer</Link>
+            <Link to="/lofi">Lofi Videos</Link>
+            {/* <Link to="/">Submit Video</Link> */}
+            {/* <Link to="/">Ambassadors</Link> */}
+            {/* <Link to="/">Agency</Link> */}
+            {/* <Link to="/">Influencer</Link> */}
           </div>
           <div class="footer-link-items">
             <h2>Social Media</h2>
-            <Link to="/">Instagram</Link>
-            <Link to="/">Facebook</Link>
             <Link to="/">Youtube</Link>
+            <Link to="/">Instagram</Link>
             <Link to="/">Twitter</Link>
+            <Link to="/">Facebook</Link>
           </div>
         </div>
       </div>
@@ -67,15 +69,15 @@ const Footer = () => {
               ILC <i className="fab fa-typo3"></i>
             </Link>
           </div>
-          <small className="website-rights">BHE ILC © 2024</small>
+          <small className="website-rights">N Cruz © 2024</small>
           <div className="social-icons">
             <Link
-              className="social-icon-link facebook"
+              class="social-icon-link youtube"
               to="/"
               target="_blank"
-              aria-label="Facebook"
+              aria-label="Youtube"
             >
-              <i className="fab fa-facebook-f"></i>
+              <i class="fab fa-youtube" />
             </Link>
             <Link
               className="social-icon-link instagram"
@@ -86,14 +88,6 @@ const Footer = () => {
               <i className="fab fa-instagram"></i>
             </Link>
             <Link
-              class="social-icon-link youtube"
-              to="/"
-              target="_blank"
-              aria-label="Youtube"
-            >
-              <i class="fab fa-youtube" />
-            </Link>
-            <Link
               class="social-icon-link twitter"
               to="/"
               target="_blank"
@@ -102,13 +96,21 @@ const Footer = () => {
               <i class="fab fa-twitter" />
             </Link>
             <Link
+              className="social-icon-link facebook"
+              to="/"
+              target="_blank"
+              aria-label="Facebook"
+            >
+              <i className="fab fa-facebook-f"></i>
+            </Link>
+            {/* <Link
               class="social-icon-link linkedin"
               to="/"
               target="_blank"
               aria-label="LinkedIn"
             >
               <i class="fab fa-linkedin" />
-            </Link>
+            </Link> */}
           </div>
         </div>
       </section>
