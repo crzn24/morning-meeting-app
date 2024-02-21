c8dc63b8e92a38050d312ef99c25b20f104a0db3
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2024-02-21 09:15:49 -0800

    Changes youtube link

--- src/components/pages/Morning.jsx
+++ src/components/pages/Morning.jsx
@@ -5,25 +5,24 @@ import ReactPlayer from "react-player";
 import { useNavigate } from "react-router-dom";
 
 const Morning = () => {
-  const navigate = useNavigate(); // Initialize useHistory hook
+  // to redirect to class page
+  const navigate = useNavigate();
 
   const handleVideoEnd = () => {
-    navigate("/class"); 
+    navigate("/class");
   };
 
   return (
     <div className="player-wrapper">
       <ReactPlayer
         className="react-player"
-        url="https://www.youtube.com/watch?v=-0o79IMmAYE&embeds_referring_euri=https%3A%2F%2Fdocs.google.com%2F&embeds_referring_origin=https%3A%2F%2Fdocs.google.com&source_ve_path=MjM4NTE&feature=emb_title"
+        url="https://youtu.be/wVBYryccseU"
         width="100%"
         height="100%"
         controls
         onEnded={handleVideoEnd}
       />
       {/* <video src={goodmorning} autoPlay /> */}
-      {/* <h1>WELCOME TO ILC</h1>
-      <p>Let's start the day!</p> */}
     </div>
   );
 };
