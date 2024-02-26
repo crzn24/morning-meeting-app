29205b72a6fe6c4f80d1260c35377110e76b738f
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2024-02-25 16:32:12 -0800

    Adds in react-calendar and displays current date

--- src/components/pages/CalendarPage.jsx
+++ src/components/pages/CalendarPage.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+// import "react-calendar/dist/Calendar.css";
+import "./CalendarPage.css";
 
 const CalendarPage = () => {
+  const [value, onChange] = useState(new Date());
+  const [date, setDate] = useState(new Date());
+  
   return (
-    <h1 className="calendar">CALENDAR</h1>
-  )
-}
+    <div className="calendarpage">
+      <div className="calendarpage-container">
+        <h1 className="calendarpage-title">CALENDAR</h1>
+        <div className="calendarpage-body">
+          <Calendar
+            onChange={onChange}
+            value={value}
+            showNeighboringMonth={false}
+            calendarType="gregory"
+            selectRange={true}
+          />
+        </div>
+        <div className="calendarpage-fulldate-wrapper">
+          <p className="calendarpage-fulldate">
+            <span>Today is</span> {date.toDateString()}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CalendarPage
+export default CalendarPage;
