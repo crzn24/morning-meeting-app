3addcfd087c8d440780ea6fca2b941d94d607b4a
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2024-03-07 17:03:47 -0800

    fixes classNames

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -28,7 +28,7 @@ const Footer = () => {
       </section>
       <div className="footer-links">
         <div className="footer-link-wrapper">
-          <div class="footer-link-items">
+          <div className="footer-link-items">
             <h2>About Us</h2>
             <Link to="/class">Our Team</Link>
             {/* <Link to="/sign-up">How it works</Link> */}
@@ -37,7 +37,7 @@ const Footer = () => {
             {/* <Link to="/">Investors</Link> */}
             <Link to="/">Terms of Service</Link>
           </div>
-          <div class="footer-link-items">
+          <div className="footer-link-items">
             <h2>Contact Us</h2>
             <Link to="/">Contact</Link>
             <Link to="/">Support</Link>
@@ -46,7 +46,7 @@ const Footer = () => {
           </div>
         </div>
         <div className="footer-link-wrapper">
-          <div class="footer-link-items">
+          <div className="footer-link-items">
             <h2>Videos</h2>
             <Link to="/lofi">Lofi Videos</Link>
             {/* <Link to="/">Submit Video</Link> */}
@@ -54,7 +54,7 @@ const Footer = () => {
             {/* <Link to="/">Agency</Link> */}
             {/* <Link to="/">Influencer</Link> */}
           </div>
-          <div class="footer-link-items">
+          <div className="footer-link-items">
             <h2>Social Media</h2>
             <Link to="/">Youtube</Link>
             <Link to="/">Instagram</Link>
@@ -76,12 +76,12 @@ const Footer = () => {
           <small className="website-rights">N Cruz © 2024</small>
           <div className="social-icons">
             <Link
-              class="social-icon-link youtube"
+              className="social-icon-link youtube"
               to="/"
               target="_blank"
               aria-label="Youtube"
             >
-              <i class="fab fa-youtube" />
+              <i className="fab fa-youtube" />
             </Link>
             <Link
               className="social-icon-link instagram"
@@ -92,12 +92,12 @@ const Footer = () => {
               <i className="fab fa-instagram"></i>
             </Link>
             <Link
-              class="social-icon-link twitter"
+              className="social-icon-link twitter"
               to="/"
               target="_blank"
               aria-label="Twitter"
             >
-              <i class="fab fa-twitter" />
+              <i className="fab fa-twitter" />
             </Link>
             <Link
               className="social-icon-link facebook"
@@ -108,12 +108,12 @@ const Footer = () => {
               <i className="fab fa-facebook-f"></i>
             </Link>
             {/* <Link
-              class="social-icon-link linkedin"
+              className="social-icon-link linkedin"
               to="/"
               target="_blank"
               aria-label="LinkedIn"
             >
-              <i class="fab fa-linkedin" />
+              <i className="fab fa-linkedin" />
             </Link> */}
           </div>
         </div>
