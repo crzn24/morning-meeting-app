a1101533cf33ff6c8eb884e55b6b537fdaa60187
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2024-03-10 20:56:30 -0700

    adds youtube playlist player

--- src/components/pages/LofiPage.jsx
+++ src/components/pages/LofiPage.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import "./LofiPage.css";
+import ReactPlayer from "react-player";
 
 const LofiPage = () => {
   return (
-    <h1 className="lofipage">LOFI</h1>
-  )
-}
+    <div className="lofipage">
+      <div className="lofi-player-wrapper">
+        <ReactPlayer
+          className="lofi-react-player"
+          url="https://youtube.com/playlist?list=PLOseBaVQKym0-ZUXIHYlXV23jN90XwjAJ&si=jtt5u8W5inv3n2MH"
+          width="100%"
+          height="100%"
+          controls
+        />
+      </div>
+    </div>
+  );
+};
 
-export default LofiPage
+export default LofiPage;
