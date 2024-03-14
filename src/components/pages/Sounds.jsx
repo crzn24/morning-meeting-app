8a96ce77283b5b809ee295c1e5a85e98ab8a442e
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2024-03-13 20:52:23 -0700

    works on soundboard styling

--- src/components/pages/Sounds.jsx
+++ src/components/pages/Sounds.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React from "react";
 import "./Sounds.css";
 
 // Import button images
-import button1Image from '../../assets/sounds/IG12yes.gif';
-import button2Image from '../../assets/sounds/IG12no.gif';
+import button1Image from "../../assets/sounds/IG12yes.gif";
+import button2Image from "../../assets/sounds/IG12no.gif";
+import button3Image from "../../assets/sounds/hellothere.gif";
+import button4Image from "../../assets/sounds/vaderno.gif";
+import button5Image from "../../assets/sounds/dramaticchipmunk.png";
+import button6Image from "../../assets/sounds/spongebob-fail.jpg";
+import button7Image from "../../assets/sounds/wrongbuzzer.jpg";
+import button8Image from "../../assets/sounds/sadsong.jpg";
 
 // Import audio files
-import button1Audio from '../../assets/sounds/IG12yes.mp3';
-import button2Audio from '../../assets/sounds/IG12no.mp3';
+import button1Audio from "../../assets/sounds/IG12yes.mp3";
+import button2Audio from "../../assets/sounds/IG12no.mp3";
+import button3Audio from "../../assets/sounds/hellothere.mp3";
+import button4Audio from "../../assets/sounds/vaderno.mp3";
+import button5Audio from "../../assets/sounds/dundundun.mp3";
+import button6Audio from "../../assets/sounds/spongebob-fail.mp3";
+import button7Audio from "../../assets/sounds/wrongbuzzer.mp3";
+import button8Audio from "../../assets/sounds/sadsong.mp3";
 
 const Sounds = () => {
   // Function to handle button click
@@ -20,14 +32,24 @@ const Sounds = () => {
   const buttonImages = {
     button1: button1Image,
     button2: button2Image,
-    // Add more button images as needed
+    button3: button3Image,
+    button4: button4Image,
+    button5: button5Image,
+    button6: button6Image,
+    button7: button7Image,
+    button8: button8Image,
   };
 
   // Define audio files
   const audioFiles = {
     button1: button1Audio,
     button2: button2Audio,
-    // Add more audio files as needed
+    button3: button3Audio,
+    button4: button4Audio,
+    button5: button5Audio,
+    button6: button6Audio,
+    button7: button7Audio,
+    button8: button8Audio,
   };
 
   return (
@@ -36,20 +58,67 @@ const Sounds = () => {
       <div className="soundboard">
         <button
           className="soundboard-button"
-          onClick={() => handleButtonClick('button1')}
+          onClick={() => handleButtonClick("button1")}
         >
-          <img src={buttonImages['button1']} alt="Button 1" />
+          <img src={buttonImages["button1"]} alt="Button 1" />
         </button>
         <button
           className="soundboard-button"
-          onClick={() => handleButtonClick('button2')}
+          onClick={() => handleButtonClick("button2")}
         >
-          <img src={buttonImages['button2']} alt="Button 2" />
+          <img src={buttonImages["button2"]} alt="Button 2" />
         </button>
-        {/* Add more buttons as needed */}
+        <button
+          className="soundboard-button"
+          onClick={() => handleButtonClick("button3")}
+        >
+          <img src={buttonImages["button3"]} alt="Button 3" />
+        </button>
+        <button
+          className="soundboard-button"
+          onClick={() => handleButtonClick("button4")}
+        >
+          <img src={buttonImages["button4"]} alt="Button 4" />
+        </button>
+        <button
+          className="soundboard-button"
+          onClick={() => handleButtonClick("button5")}
+        >
+          <img src={buttonImages["button5"]} alt="Button 5" />
+        </button>
+        <button
+          className="soundboard-button"
+          onClick={() => handleButtonClick("button6")}
+        >
+          <img src={buttonImages["button6"]} alt="Button 6" />
+        </button>
+        <button
+          className="soundboard-button"
+          onClick={() => handleButtonClick("button7")}
+        >
+          <img src={buttonImages["button7"]} alt="Button 7" />
+        </button>
+        <button
+          className="soundboard-button"
+          onClick={() => handleButtonClick("button8")}
+        >
+          <img src={buttonImages["button8"]} alt="Button 8" />
+        </button>
+        {/* <button
+          className="soundboard-button"
+          onClick={() => handleButtonClick("button9")}
+        >
+          <img src={buttonImages["button9"]} alt="Button 9" />
+        </button>
+        <button
+          className="soundboard-button"
+          onClick={() => handleButtonClick("button10")}
+        >
+          <img src={buttonImages["button10"]} alt="Button 10" />
+        </button> */}
       </div>
     </div>
   );
-}
+};
 
 export default Sounds;
