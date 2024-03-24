ad23a9c666df7f9119103880330c40ddbacccfbe
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2024-03-24 13:35:47 -0700

    adds video element

--- src/components/pages/SelPage.jsx
+++ src/components/pages/SelPage.jsx
@@ -1,11 +1,40 @@
 import React, { useState } from "react";
 import "./SelPage.css";
+import ReactPlayer from "react-player";
 
 const SelPage = () => {
   // State to keep track of marker positions
   const [markers, setMarkers] = useState([]);
-  const colors = ['#ff5733', '#33ff57', '#5733ff', '#ff5733', '#33ff57', '#5733ff', '#ff33ff', '#33ffff', '#ffff33', '#3333ff', '#ff6633', '#33ff66', '#6633ff', '#ff3366', '#66ff33', '#3366ff', '#ff9933', '#33ff99', '#9933ff', '#ff3399', '#99ff33', '#3399ff', '#ffcc33', '#33ffcc', '#cc33ff', '#ff33cc', '#ccff33', '#33ccff'];
-
+  const colors = [
+    "#ff5733",
+    "#33ff57",
+    "#5733ff",
+    "#ff5733",
+    "#33ff57",
+    "#5733ff",
+    "#ff33ff",
+    "#33ffff",
+    "#ffff33",
+    "#3333ff",
+    "#ff6633",
+    "#33ff66",
+    "#6633ff",
+    "#ff3366",
+    "#66ff33",
+    "#3366ff",
+    "#ff9933",
+    "#33ff99",
+    "#9933ff",
+    "#ff3399",
+    "#99ff33",
+    "#3399ff",
+    "#ffcc33",
+    "#33ffcc",
+    "#cc33ff",
+    "#ff33cc",
+    "#ccff33",
+    "#33ccff",
+  ];
 
   // Function to handle marker drag
   const handleDragStart = (event, index) => {
@@ -38,17 +67,37 @@ const SelPage = () => {
   };
 
   return (
-    <div className="emotions-chart" onClick={addMarker} onDragOver={(event) => event.preventDefault()} onDrop={handleDrop}>
-      {/* Render markers */}
-      {markers.map((marker, index) => (
+    <div className="sel-container">
+      <div className="sel-content-wrapper">
+        <div className="sel-react-player-wrapper">
+          <ReactPlayer
+            className="sel-react-player"
+            url="https://youtu.be/l-nXHtfAgY0"
+            controls
+          />
+        </div>
         <div
-          key={index}
-          className="marker"
-          style={{ left: marker.x, top: marker.y, backgroundColor: marker.color }}
-          draggable="true"
-          onDragStart={(event) => handleDragStart(event, index)}
-        />
-      ))}
+          className="emotions-chart"
+          onClick={addMarker}
+          onDragOver={(event) => event.preventDefault()}
+          onDrop={handleDrop}
+        >
+          {/* Render markers */}
+          {markers.map((marker, index) => (
+            <div
+              key={index}
+              className="marker"
+              style={{
+                left: marker.x,
+                top: marker.y,
+                backgroundColor: marker.color,
+              }}
+              draggable="true"
+              onDragStart={(event) => handleDragStart(event, index)}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
